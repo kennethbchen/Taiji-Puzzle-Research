495555ad89445bc9c51691f41d4a567badb152df
kennethbchen <kennethbchen@gmail.com>
2023-09-30 19:07:28 -0400

    Implemented showing symbols and adding / removing symbols interactively

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -1,9 +1,20 @@
-from vedo import show, Spheres, Lines
-
-
+from vedo import show, Spheres, Lines, Plotter, Sphere, Line
 from Node import Node
 from Edge import Edge
 
+"""
+    Coordinate System:
+    +x - right
+    +y - up
+    +z - backwards
+    
+    In relation to layer data:
+    x-axis: columns
+    y-axis: layers
+    z-axis: rows
+    
+    (x, y, z) -> (column, layer, row)
+"""
 layers = [
     [
         [1, 1, 1, 1, 1, 4],
@@ -50,11 +61,17 @@ layerss = [
 
 layer_colors = ["red", "green", "blue"]
 
+dimensions = {
+    "rows": len(layers[0]),
+    "cols": len(layers[0][0]),
+    "layers": len(layers)
+}
+
 node_data = {}
 edge_data = []
-symbol_data = [(0, 1), (1, 1)]
+symbol_data = [(3, 0), (4, 0), (2, 2), (3, 2), (0, 3), (2, 3), (2, 4), (1, 5)]
 
-spacing = 1.5
+
 
 # Setup
 for layer_i, layer in enumerate(layers):
@@ -86,6 +103,62 @@ for i, (key, tile) in enumerate(node_data.items()):
 
 
 # Visualize
+plot = Plotter(axes=1, bg="gray")
+
+
+def on_mouse_click(event):
+    mesh = event.actor
+
+    if not mesh:
+        return
+
+    if not add_symbol(mesh.pos()[0], mesh.pos()[2]):
+        print("removed")
+        remove_symbol(mesh.pos()[0], mesh.pos()[2])
+
+    print(symbols)
+    plot.render()
+
+def on_mouse_move(event):
+    mesh = event.actor
+
+    if not mesh:
+        plot.remove('silu')
+        plot.render()
+        return
+
+    # Highlight selected
+    sil = event.actor.silhouette().linewidth(6).c('white')
+    sil.name = "silu"
+    plot.remove('silu').add(sil)
+    plot.render()
+
+symbols = {}
+
+def remove_symbol(row, col):
+
+    if ((row, col)) not in symbols:
+        return False
+
+    symbol = symbols.pop((row, col))
+    plot.remove(symbol)
+
+def add_symbol(row, col):
+
+    if ((row, col)) in symbols:
+        return False
+
+    line_start = (row, 0, col)
+    line_end = (row, dimensions["layers"] - 0.5, col)
+
+    l = Line(p0=line_start, p1=line_end, res=0, lw=5, c="white", alpha=1)
+    symbols[(row, col)] = l
+
+    plot.add(l)
+    return True
+
+for symbol_coord in symbol_data:
+    add_symbol(symbol_coord[0], symbol_coord[1])
 
 n_centers = []
 n_colors = []
@@ -101,7 +174,18 @@ for edge in edge_data:
     e_colors.append('white')
 
 edges = Lines(start_pts=e_endpoints, res=0, lw=5, c="black", alpha=1)
-nodes = Spheres(centers=n_centers, r=0.2, c=n_colors, alpha=1)
 
-show(edges, nodes, at=0, axes=1, bg="gray").interactive().close()
+nodes = []
+
+for i in range(len(n_centers)):
+    s = Sphere(r=0.1, alpha=1).pos(n_centers[i]).color(n_colors[i])
+    s.name = f"sphere nr.{i} at {n_centers[i]}"
+    nodes.append(s)
+
+
+
+plot.add_callback('mouse move', on_mouse_move)
+plot.add_callback("mouse click", on_mouse_click)
+
+plot.show(edges, nodes, list(symbols.values()), at=0).interactive().close()
 exit()
