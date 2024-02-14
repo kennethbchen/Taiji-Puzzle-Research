ee2a5d8a726078b85fcdcd1db01444d2219226e5
kennethbchen <kennethbchen@gmail.com>
2024-02-14 13:17:07 -0500

    Implemented creating diagonals in sudoku_to_sgp

--- reducers/sudoku_to_sgp.py
+++ reducers/sudoku_to_sgp.py
@@ -1,28 +1,41 @@
+import numpy
 from sudoku import Sudoku
 
 from SGP import SGP
 import numpy as np
 
-puzzle = Sudoku(2,2).difficulty(0.5)
+puzzle = Sudoku(3).difficulty(0.5)
 
 def sudoku_to_SGP(sudoku_puzzle):
-    #print(sudoku_puzzle)
 
-    # Arrays of boards where each number is a distinct region
-    sgp_boards = []
+    sgp_size = sudoku_puzzle.size * 2
+    # Array of 4 boards where each number in a board is a distinct region
+    sgp_boards = np.ndarray((4, sgp_size, sgp_size), dtype=int)
+
 
     # Rows
-    sgp_boards.append(np.transpose(np.full( (sudoku_puzzle.size, sudoku_puzzle.size), np.arange(0, sudoku_puzzle.size))))
+    sgp_boards[0] = np.transpose(np.full( (sgp_size, sgp_size), np.arange(0, sgp_size)))
 
     # Columns
-    sgp_boards.append(np.transpose(sgp_boards[0]))
+    sgp_boards[1] = (np.transpose(sgp_boards[0]))
 
     # Diagonals
 
+    dia = numpy.full((sgp_size, sgp_size), numpy.arange(sgp_size, sgp_size * 2))
+
+    for i in range(sgp_size):
+        dia[i] -= i
+
+    sgp_boards[2] = dia
+    sgp_boards[3] = (np.flip(dia, 0))
+
+    # n x m blocks
+
 
     print(sudoku_puzzle)
-    print(sudoku_puzzle.height, sudoku_puzzle.width)
+    print(sgp_size)
     print(sgp_boards)
+    #print(sgp_boards.tolist())
 
     sgp_hint = np.tile(np.array(sudoku_puzzle.board), (2,2))
 
