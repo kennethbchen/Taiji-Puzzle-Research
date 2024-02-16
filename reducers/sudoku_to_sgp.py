5b8417754068e225763deae56ae84e72b94c9da6
kennethbchen <kennethbchen@gmail.com>
2024-02-16 13:20:02 -0500

    Implemented creating boards for arbitrary sudoku puzzle

--- reducers/sudoku_to_sgp.py
+++ reducers/sudoku_to_sgp.py
@@ -4,10 +4,10 @@ from sudoku import Sudoku
 from SGP import SGP
 import numpy as np
 
-puzzle = Sudoku(3).difficulty(0.5)
+puzzle = Sudoku(3, 2).difficulty(0.5)
 
 def sudoku_to_SGP(sudoku_puzzle):
-
+    print(sudoku_puzzle)
     sgp_size = sudoku_puzzle.size * 2
     # Array of 4 boards where each number in a board is a distinct region
     sgp_boards = np.zeros((4, sgp_size, sgp_size))
@@ -21,9 +21,33 @@ def sudoku_to_SGP(sudoku_puzzle):
 
     # n x m blocks
 
+    # Vertical Checkerboard
 
-    print(sudoku_puzzle)
-    print(sgp_size)
+    # Dimensions of a checkerboard where the grid height > width
+    v_check_rows = int(sgp_size / sudoku_puzzle.height / 2)
+    v_check_height = sudoku_puzzle.height * 2
+    v_check_cols = sgp_size
+    v_check_width = sudoku_puzzle.width
+
+    checker_col = np.zeros(( v_check_rows, v_check_height, v_check_cols))
+
+    checker_col = checker_col.reshape(v_check_rows, v_check_cols * v_check_height)
+
+    # Add checkerboard row id offset
+    checker_col += np.arange(0, v_check_cols, v_check_height).reshape(v_check_rows, -1)
+
+    checker_col = checker_col.reshape(v_check_rows, v_check_height, v_check_cols)
+
+    # Add checkerboard column id offset
+    temp = np.broadcast_to(np.arange(0, v_check_cols / v_check_width).reshape(-1, 1), ( int(v_check_cols / v_check_width) , v_check_width)).flatten()
+
+    checker_col = (checker_col + temp).reshape(sgp_size, sgp_size)
+
+    sgp_boards[2] = checker_col
+
+    # Horizontal Checkerboard
+    sgp_boards[3] = np.transpose(checker_col)
+
     print(sgp_boards)
     #print(sgp_boards.tolist())
 
