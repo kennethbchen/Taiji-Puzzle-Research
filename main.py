737e4fa093f7447d67b4ae59bc27892592cba9f8
kennethbchen <kennethbchen@gmail.com>
2024-02-26 12:02:34 -0500

    Implemented way to check if a sudoku solution is tiled

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from sudoku import Sudoku
 
 from puzzles import puzzles
 from SGP import SGP
-from reducers.sudoku_to_sgp import sudoku_to_SGP
+from reducers.sudoku_to_sgp import sudoku_to_SGP, is_solution_tiled,get_solution_tile
 from sgp_solver import Solve_SGP
 from visualizer.visualizer import visualize_SGP
 
-sudoku_puzzle = Sudoku(3, 3).difficulty(0.5)
+sudoku_puzzle = Sudoku(2, 3).difficulty(0.5)
 sudoku_puzzle.show()
 
 print("Solved Sudoku:")
@@ -19,10 +19,20 @@ puzzle = sudoku_to_SGP(sudoku_puzzle)
 
 solutions = Solve_SGP(puzzle, get_all=True, log_progress=False)
 
+# Problems:
+# Not every solution is tiled
+# Not every "solution" is valid sudoku
+
 for solution_df in solutions:
     # Sort the df so the solution is read in reading order
     # Get only the color number
     # Convert from 0 indexing to 1 indexing
+
     solution = solution_df.sort_values(by=["row", "col"]).values.reshape((solution_df["row"].max() + 1, -1, 3))[:, :, 2] + 1
-    print("Solved SGP:")
-    print(solution)
+    if is_solution_tiled(solution):
+        print("Solved Sudoku:")
+        print(get_solution_tile(solution))
+    else:
+        print("Untiled Solution")
+
+
--- reducers/sudoku_to_sgp.py
+++ reducers/sudoku_to_sgp.py
@@ -51,3 +51,40 @@ def sudoku_to_SGP(sudoku_puzzle):
     sgp_hint = np.tile(np.array(sudoku_puzzle.board), (2,2))
 
     return SGP(sgp_symbols, sgp_boards.tolist(), solution_hints=sgp_hint)
+
+
+def get_solution_tile(solution, tile=(0, 0)):
+    # Assumes rectangular solution size
+
+    # (row, col)
+    tile_size = (int(len(solution) / 2), int(len(solution[0]) / 2))
+
+    tile_offset = (tile[0] * tile_size[0], tile[1] * tile_size[1])
+
+    return solution[tile_offset[0]: (tile_offset[0] + tile_size[0]), tile_offset[1]: (tile_offset[1] + tile_size[1])]
+
+
+def is_solution_tiled(solution):
+    # Assumes rectangular solution size
+
+    # (row, col)
+    tile_size = (int(len(solution)/2), int(len(solution[0])/2))
+
+    tile_positions = [(0, 0), (0, 1), (1, 0), (1, 1)]
+
+    origin_tile = None
+    for tile in tile_positions:
+        tile_offset = (tile[0] * tile_size[0], tile[1] * tile_size[1])
+
+        current_tile_data = solution[tile_offset[0]: (tile_offset[0] + tile_size[0]), tile_offset[1]: (tile_offset[1] + tile_size[1])]
+        if origin_tile is None:
+            origin_tile = current_tile_data
+
+        if not (current_tile_data & origin_tile).all():
+            print("Origin tile and", tile, "are not the same")
+            print(origin_tile)
+            print("vs")
+            print(current_tile_data)
+            return False
+
+    return True
